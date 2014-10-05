71f43c7177cf8ee98eda8e986f9eb1f5e8488c7c
GovernorFancyPants <tobiasjohansson123@gmail.com>
2014-10-06 00:11:14 +0200

    Switch fix

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var switch1 = new rageSwitch(document.getElementById('switch1'));
 var switch2 = new rageSwitch(document.getElementById('switch2'));
+var switch3 = new rageSwitch(document.getElementById('switch3'));
+var switch4 = new rageSwitch(document.getElementById('switch4'));
 
 function rageSwitch(el) {
     el.addEventListener('click', function() {
